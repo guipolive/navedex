4031e3b277331e0eb58dda416666c58f3af295ac
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2021-03-10 18:41:48 -0300

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
 		return(
 			<Html>
 				<Head>
-					<link rel="shortcut icon" href="favicon.png" type="image/png" />
+					<link rel="shortcut icon" href="/favicon.ico" type="image/png" />
 
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet" />
